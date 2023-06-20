69655f38d83f7b3b8a99be4eecac2ec841521f8e
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2023-06-20 16:35:16 -0300

    create reset password and send email

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,10 +4,26 @@ import { UsersController } from './users.controller';
 import { PrismaService } from 'src/database/prisma.service';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersPrismaRepository } from './repositories/prisma/users-prisma.repository';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MailService } from 'src/utils/mail.service';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: `"No Reply" <${process.env.SMTP_USER}>`,
+      }
+    })
+  ],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, {
+  providers: [UsersService, PrismaService, MailService, {
     provide: UsersRepository, useClass: UsersPrismaRepository
   }],
   exports: [UsersService]
---
+++ src/utils/mail.service.ts
@@ -0,0 +1,64 @@
+import { MailerService } from "@nestjs-modules/mailer";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { SendEmailDto } from "src/modules/users/dto/send-email.dto";
+import * as Mailgen from "mailgen"
+
+const mailGenerator = new Mailgen({
+  theme: "default",
+  product: {
+    name: "Motor Shop Kenzie",
+    link: "http://localhost:3001"
+  }
+})
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) { }
+
+  async sendEmail({ to, subject, text }: SendEmailDto) {
+    await this.mailerService.sendMail({
+      to,
+      subject,
+      html: text
+    })
+      .then(() => {
+        console.log("Email enviado com sucesso")
+      })
+      .catch((err) => {
+        console.log(err)
+        throw new InternalServerErrorException("Erro ao enviar o email, tente novamente mais tarde")
+      })
+
+  }
+  resetPasswordTemplate(userEmail: string, userName: string, resetToken: string) {
+    const email = {
+      body: {
+        name: userName,
+        intro: `Olá ${userName},`,
+        action: {
+          instructions: "Clique no botão abaixo para criar uma nova senha",
+          button: {
+            text: "Resetar senha",
+            link: `http://localhost:3000/users/resetPassword/${resetToken}`
+          }
+        }
+      }
+
+    }
+
+    const emailBody = mailGenerator.generate(email)
+
+    const emailTemplate = {
+      to: userEmail,
+      subject: "Reset Password Motors Shop",
+      text: emailBody
+    }
+
+    return emailTemplate
+
+  }
+
+
+
+}
+
